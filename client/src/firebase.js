84547ba81650939c48d33d914d397c412beda64c
Deshan Lanka <deshanlanka500@gmail.com>
2024-11-28 20:44:17 +0530

    Update FireBase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-stack-project-4eb3d.firebaseapp.com",
-  projectId: "mern-stack-project-4eb3d",
-  storageBucket: "mern-stack-project-4eb3d.firebasestorage.app",
-  messagingSenderId: "950272834292",
-  appId: "1:950272834292:web:2079939302324eb3003ca1"
+  authDomain: "mern-estate.firebaseapp.com",
+  projectId: "mern-estate",
+  storageBucket: "mern-estate.appspot.com",
+  messagingSenderId: "1078482850952",
+  appId: "1:1078482850952:web:28f19139ab77246602fb3d"
 };
 
 // Initialize Firebase
